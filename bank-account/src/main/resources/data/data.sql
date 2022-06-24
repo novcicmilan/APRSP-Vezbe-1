6f681029a1811b05d56bca247f8aa815ce03430b
Novcic Milan <novcic.milan17@uns.ac.rs>
2022-06-24 15:28:59 +0200

    data sql

---
+++ bank-account/src/main/resources/data/data.sql
@@ -0,0 +1,5 @@
+insert into bank_accounts(id, usd, gbp, chf, eur, rsd, email_address) 
+values(1000, 1000, 1000, 1000, 1000, 1000, 'user1@email.com'),
+		(1001, 1000, 1000, 1000, 1000, 1000, 'user2@email.com'),
+		(1002, 1000, 1000, 1000, 1000, 1000, 'user3@email.com'),
+		(1003, 1000, 1000, 1000, 1000, 1000, 'user4@email.com');
